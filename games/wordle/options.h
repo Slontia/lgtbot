c0c44769177b48e3067db19b880c40299e835898
jeffxzy <xuzhengyangjeff@126.com>
2022-09-16 01:42:57 +0800

    wordle: change round time

--- games/wordle/options.h
+++ games/wordle/options.h
@@ -1,2 +1,2 @@
 GAME_OPTION("回合数", 回合数, (ArithChecker<uint32_t>(1, 20, "回合数")), 40)
-GAME_OPTION("每回合时间限制", 时限, (ArithChecker<uint32_t>(10, 3600, "超时时间（秒）")), 150)
+GAME_OPTION("每回合时间限制", 时限, (ArithChecker<uint32_t>(10, 3600, "超时时间（秒）")), 180)
