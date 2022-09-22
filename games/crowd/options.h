4a88bbadd35182723d0198e91d67d1d6bcd2d23b
jeffxzy <xuzhengyangjeff@126.com>
2022-09-22 19:00:49 +0800

    crowd: add new questions

--- games/crowd/options.h
+++ games/crowd/options.h
@@ -1,2 +1,3 @@
 GAME_OPTION("回合数", 回合数, (ArithChecker<uint32_t>(1, 20, "回合数")), 7)
 GAME_OPTION("每回合时间限制", 时限, (ArithChecker<uint32_t>(10, 3600, "超时时间（秒）")), 120)
+GAME_OPTION("测试", 测试, (ArithChecker<uint32_t>(1, 999, "测试")), 99999)
