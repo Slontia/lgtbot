5de0d4ec180a8348f44f59a06f77af649a357558
Dva <dvacheng@gmail.com>
2022-09-22 23:18:25 +0800

    nerduel: new game 1.0 beta

---
+++ games/nerduel/unittest.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2018-present, Chang Liu <github.com/slontia>. All rights reserved.
+//
+// This source code is licensed under LGPLv2 (found in the LICENSE file).
+
+#include "game_framework/unittest_base.h"
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
