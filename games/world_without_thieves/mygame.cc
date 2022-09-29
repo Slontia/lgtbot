e87d9ca2e461630b8773197afeaa00b4789f703e
jeffxzy <xuzhengyangjeff@126.com>
2022-09-29 16:04:15 +0800

    wwt dvalue: change copyright

--- games/world_without_thieves/mygame.cc
+++ games/world_without_thieves/mygame.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-present, Chang Liu <github.com/slontia>. All rights reserved.
+// Copyright (c) 2018-present, ZhengYang Xu <github.com/jeffxzy>. All rights reserved.
 //
 // This source code is licensed under LGPLv2 (found in the LICENSE file).
 
