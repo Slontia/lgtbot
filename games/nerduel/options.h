372d313b600601366efe043f898a36e1f6f65c3d
程元 <yuan.cheng@mihoyo.com>
2022-10-01 21:27:46 +0800

    Nerdual: update time limit option & third_party.

--- games/nerduel/options.h
+++ games/nerduel/options.h
@@ -1,2 +1,3 @@
 GAME_OPTION("等式长度", 等式长度, (ArithChecker<uint32_t>(5, 9, "长度")), 7)
 GAME_OPTION("游戏模式", 游戏模式, (BoolChecker("标准", "狂野")), true)
+GAME_OPTION("时限", 时限, (ArithChecker<uint32_t>(20, 300, "时间限制（秒）")), 180)
