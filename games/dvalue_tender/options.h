9d42f8dacda7234ae0c17e998cd2c79495a1d2aa
jeffxzy <xuzhengyangjeff@126.com>
2022-10-07 00:24:00 +0800

    dvalue: add init coins option

--- games/dvalue_tender/options.h
+++ games/dvalue_tender/options.h
@@ -1,2 +1,3 @@
 GAME_OPTION("回合数", 回合数, (ArithChecker<uint32_t>(1, 20, "回合数")), 15)
+GAME_OPTION("金币", 金币, (ArithChecker<uint32_t>(10, 1000000, "金币")), 30)
 GAME_OPTION("每回合时间限制", 时限, (ArithChecker<uint32_t>(10, 3600, "超时时间（秒）")), 90)
