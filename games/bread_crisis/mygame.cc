7d2c88a8b20593409bcb5e03d9ff020d4ba10311
程元 <yuan.cheng@mihoyo.com>
2022-10-23 13:27:04 +0800

    bread_crisis: new game

---
+++ games/bread_crisis/mygame.cc
@@ -0,0 +1,414 @@
+#include <array>
+#include <functional>
+#include <memory>
+#include <set>
+
+#include "game_framework/game_main.h"
+#include "game_framework/game_options.h"
+#include "game_framework/game_stage.h"
+#include "utility/html.h"
+#include "utility/msg_checker.h"
+
+const std::string k_game_name =
+    "面包危机";                   // the game name which should be unique among all the games
+const uint64_t k_max_player = 0;  // 0 indicates no max-player limits
+const uint64_t k_multiple = 1;    // the default score multiple for the game, 0 for a testing game,
+// 1 for a formal game, 2 or 3 for a long formal game
+const std::string k_developer = "dva";
+const std::string k_description = "保存体力，尽可能活到最后的游戏";
+
+std::string GameOption::StatusInfo() const {
+  return "最多 " + std::to_string(GET_VALUE(回合数)) + " 回合，每回合超时时间 " +
+         std::to_string(GET_VALUE(时限)) + " 秒";
+}
+
+bool GameOption::ToValid(MsgSenderBase& reply) {
+  if (PlayerNum() < 3) {
+    reply() << "该游戏至少 3 人参加，当前玩家数为 " << PlayerNum();
+    return false;
+  }
+  return true;
+}
+
+uint64_t GameOption::BestPlayerNum() const { return 6; }
+
+// ========== GAME STAGES ==========
+
+class RoundStage;
+
+class MainStage : public MainGameStage<RoundStage> {
+ public:
+  MainStage(const GameOption& option, MatchBase& match)
+      : GameStage(
+            option, match,
+            MakeStageCommand("查看当前游戏进展情况", &MainStage::Status_, VoidChecker("赛况"))),
+        round_(0),
+        alive_(0),
+        player_scores_(option.PlayerNum(), 0),
+        player_hp_(option.PlayerNum(), 30),
+        player_select_(option.PlayerNum(), 'N'),
+        player_last_(option.PlayerNum(), 'N'),
+        player_target_(option.PlayerNum(), 0),
+        player_number_(option.PlayerNum(), 0),
+        player_eli_(option.PlayerNum(), 0) {}
+
+  virtual VariantSubStage OnStageBegin() override;
+  virtual VariantSubStage NextSubStage(RoundStage& sub_stage, const CheckoutReason reason) override;
+
+  virtual int64_t PlayerScore(const PlayerID pid) const override { return player_scores_[pid]; }
+  std::vector<int64_t> player_scores_;
+
+  //生命值
+  std::vector<int64_t> player_hp_;
+  //选择
+  std::vector<char> player_select_;
+  //上次选择
+  std::vector<char> player_last_;
+  //选择的玩家
+  std::vector<int64_t> player_target_;
+  //抢夺数量
+  std::vector<int64_t> player_number_;
+  //已经被淘汰
+  std::vector<int64_t> player_eli_;
+
+  int alive_;
+  std::string Pic = "";
+  std::string GetName(std::string x);
+
+ private:
+  CompReqErrCode Status_(const PlayerID pid, const bool is_public, MsgSenderBase& reply) {
+    //        reply() << "当前游戏情况：未设置";
+
+    std::string PreBoard = "";
+    PreBoard += "本局玩家序号如下：\n";
+    for (int i = 0; i < option().PlayerNum(); i++) {
+      PreBoard += std::to_string(i + 1) + " 号：" + PlayerName(i);
+
+      if (i != (int)option().PlayerNum() - 1) {
+        PreBoard += "\n";
+      }
+    }
+
+    Boardcast() << PreBoard;
+
+    std::string WordSitu = "";
+    WordSitu += "当前玩家状态如下：\n";
+    for (int i = 0; i < option().PlayerNum(); i++) {
+      WordSitu += std::to_string(i + 1) + " 号：";
+
+      WordSitu += std::to_string(player_hp_[i]) + "体力 ";
+      if (player_select_[i] == 'N') WordSitu += "民  " + std::to_string(player_number_[i]);
+      if (player_select_[i] == 'S')
+        WordSitu +=
+            "贼" + std::to_string(player_target_[i]) + "  " + std::to_string(player_number_[i]);
+
+      if (i != (int)option().PlayerNum() - 1) {
+        WordSitu += "\n";
+      }
+    }
+
+    //        select is updated unexpectedly, which may cause error, so use Pic instead now.
+    //        Boardcast() << WordSitu;
+
+    Boardcast() << Markdown(Pic + "</table>");
+
+    // Returning |OK| means the game stage
+    return StageErrCode::OK;
+  }
+
+  int round_;
+};
+
+class RoundStage : public SubGameStage<> {
+ public:
+  RoundStage(MainStage& main_stage, const uint64_t round)
+      : GameStage(
+            main_stage, "第 " + std::to_string(round) + " 回合",
+            MakeStageCommand("获取面包", &RoundStage::Select_N_,
+                             ArithChecker<int64_t>(1, 5, "面包数量")),
+            MakeStageCommand("抢夺面包", &RoundStage::Select_S_,
+                             ArithChecker<int64_t>(1, main_stage.option().PlayerNum(), "抢夺目标"),
+                             ArithChecker<int64_t>(1, 5, "面包数量"))) {}
+
+  virtual void OnStageBegin() override {
+    StartTimer(option().GET_VALUE(时限));
+
+    Boardcast() << name() << "，请玩家私信选择身份。";
+  }
+
+  virtual CheckoutErrCode OnTimeout() override {
+    //        Boardcast() << name() << "超时结束";
+
+    for (int i = 0; i < option().PlayerNum(); i++) {
+      //             Boardcast()<<std::to_string(IsReady(i))<<std::to_string(main_stage().player_eli_[i]);
+      if (IsReady(i) == false && !main_stage().player_eli_[i]) {
+        Boardcast() << "玩家 " << main_stage().PlayerName(i) << " 超时仍未行动，已被淘汰";
+        main_stage().player_hp_[i] = 0;
+        main_stage().player_select_[i] = 'N';
+        main_stage().player_number_[i] = rand() % 5 + 1;
+        main_stage().player_target_[i] = 0;
+      }
+    }
+
+    RoundStage::calc();
+    // Returning |CHECKOUT| means the current stage will be over.
+    return StageErrCode::CHECKOUT;
+  }
+
+  virtual CheckoutErrCode OnPlayerLeave(const PlayerID pid) override {
+    int i = pid;
+    //        Boardcast() << PlayerName(i) << "退出游戏";
+    main_stage().player_hp_[i] = 0;
+    main_stage().player_select_[i] = 'N';
+    main_stage().player_number_[i] = rand() % 5 + 1;
+    main_stage().player_target_[i] = 0;
+    // Returning |CONTINUE| means the current stage will be continued.
+    return StageErrCode::CONTINUE;
+  }
+
+  virtual AtomReqErrCode OnComputerAct(const PlayerID pid, MsgSenderBase& reply) override {
+    int i = pid;
+    main_stage().player_select_[i] = 'N';
+    main_stage().player_number_[i] = rand() % 3 + 3;
+    main_stage().player_target_[i] = 0;
+
+    return StageErrCode::READY;
+  }
+
+  virtual void OnAllPlayerReady() override {
+    Boardcast() << "所有玩家选择完成，下面公布赛况。";
+    RoundStage::calc();
+  }
+
+  void calc() {
+    // first calculate thives
+    std::vector<bool> stolen(option().PlayerNum(), false);
+    for (int i = 0; i < option().PlayerNum(); i++) {
+      if (main_stage().player_select_[i] == 'S') {
+        int target = main_stage().player_target_[i] - 1;
+        if (main_stage().player_select_[target] == 'N' &&
+            main_stage().player_number_[target] == main_stage().player_number_[i]) {
+          main_stage().player_hp_[i] += 5;
+          main_stage().player_hp_[target] = 0;
+          stolen[target] = true;
+        } else {
+          stolen[i] = true;
+        }
+      }
+    }
+
+    // finally calculate commons
+    for (int i = 0; i < option().PlayerNum(); i++) {
+      if (!stolen[i]) {
+        main_stage().player_hp_[i] += main_stage().player_number_[i];
+      }
+      main_stage().player_hp_[i] -= 5;
+    }
+
+    for (int i = 0; i < option().PlayerNum(); i++) {
+      if (main_stage().player_hp_[i] < 0) main_stage().player_hp_[i] = 0;
+    }
+
+    std::string x = "<tr>";
+    for (int i = 0; i < option().PlayerNum(); i++) {
+      x += "<td bgcolor=\"#DCDCDC\">";
+
+      if (main_stage().player_eli_[i] == 1) {
+        x += " ";
+      } else {
+        if (main_stage().player_select_[i] == 'S')
+          x += "抢夺" + std::to_string(main_stage().player_target_[i]);
+        else
+          x += "获取";
+      }
+      x += " " + std::to_string(main_stage().player_number_[i]);
+      x += "</td>";
+    }
+    x += "</tr>";
+    x += "<tr>";
+    for (int i = 0; i < option().PlayerNum(); i++) {
+      if (main_stage().player_select_[i] == 'S' && !stolen[i]) {
+        x += "<td bgcolor=\"#BAFFA8\">";
+      } else {
+        x += "<td>";
+      }
+
+      if (main_stage().player_eli_[i] == 0) {
+        x += std::to_string(main_stage().player_hp_[i]);
+      }
+
+      x += "</td>";
+    }
+    x += "</tr>";
+    main_stage().Pic += x;
+    Boardcast() << Markdown(main_stage().Pic + "</table>");
+
+    for (int i = 0; i < option().PlayerNum(); i++) {
+      if (main_stage().player_hp_[i] <= 0) {
+        main_stage().player_select_[i] = ' ';
+
+        if (main_stage().player_eli_[i] == 0) {
+          main_stage().player_eli_[i] = 1;
+          Eliminate(i);
+          main_stage().alive_--;
+        }
+      } else {
+        // Everyone gain 1 score for his survival
+        main_stage().player_scores_[i]++;
+      }
+    }
+
+    for (int i = 0; i < option().PlayerNum(); i++)
+      main_stage().player_last_[i] = main_stage().player_select_[i];
+  }
+
+ private:
+  AtomReqErrCode Select_N_(const PlayerID pid, const bool is_public, MsgSenderBase& reply,
+                           const int64_t number) {
+    if (is_public) {
+      reply() << "[错误] 请私信裁判进行选择。";
+      return StageErrCode::FAILED;
+    }
+    if (IsReady(pid)) {
+      reply() << "[错误] 您本回合已经进行过选择了。";
+      return StageErrCode::FAILED;
+    }
+    return Selected_(pid, reply, 'N', number);
+  }
+
+  AtomReqErrCode Select_S_(const PlayerID pid, const bool is_public, MsgSenderBase& reply,
+                           const int64_t target, const int64_t number) {
+    if (is_public) {
+      reply() << "[错误] 请私信裁判进行选择。";
+      return StageErrCode::FAILED;
+    }
+    if (IsReady(pid)) {
+      reply() << "[错误] 您本回合已经进行过选择了。";
+      return StageErrCode::FAILED;
+    }
+    if (target < 1 || target > option().PlayerNum() || target - 1 == pid ||
+        main_stage().player_eli_[target - 1] == 1) {
+      reply() << "[错误] 目标无效。";
+      return StageErrCode::FAILED;
+    }
+    return Selected_(pid, reply, 'S', number, target);
+  }
+
+  AtomReqErrCode Selected_(const PlayerID pid, MsgSenderBase& reply, char type,
+                           const int64_t number, const int64_t target = 0) {
+    if (type != 'S' && type != 'N' && type != 'P') {
+      Boardcast() << "Wrong input on pid = " << std::to_string(pid) << std::to_string(type)
+                  << " in f->Selected";
+    }
+    // Boardcast() << pid << " " << type << number << " " << target;
+    main_stage().player_select_[pid] = type;
+    main_stage().player_number_[pid] = number;
+    main_stage().player_target_[pid] = target;
+    //        reply() << "设置成功。";
+    // Returning |READY| means the player is ready. The current stage will be over when all
+    // surviving players are ready.
+    return StageErrCode::READY;
+  }
+};
+
+std::string MainStage::GetName(std::string x) {
+  std::string ret = "";
+  int n = x.length();
+  if (n == 0) return ret;
+
+  int l = 0;
+  int r = n - 1;
+
+  if (x[0] == '<') l++;
+  if (x[r] == '>') {
+    while (r >= 0 && x[r] != '(') r--;
+    r--;
+  }
+
+  for (int i = l; i <= r; i++) {
+    ret += x[i];
+  }
+  return ret;
+}
+
+MainStage::VariantSubStage MainStage::OnStageBegin() {
+  srand((unsigned int)time(NULL));
+  alive_ = option().PlayerNum();
+
+  Pic += "<table><tr>";
+  for (int i = 0; i < option().PlayerNum(); i++) {
+    Pic += "<th >" + std::to_string(i + 1) + " 号： " + GetName(PlayerName(i)) + "　</th>";
+    if (i % 4 == 3) Pic += "</tr><tr>";
+  }
+  Pic += "</tr><br>";
+
+  Pic += "<table style=\"text-align:center\"><tbody>";
+  Pic += "<tr>";
+  for (int i = 0; i < option().PlayerNum(); i++) {
+    //        if(i%2)
+    //            Pic+="<th>";
+    //        else
+    Pic += "<th bgcolor=\"#FFE4C4\">";
+
+    // Please note that this is not 2 spaces but a special symbol.
+    Pic += "　" + std::to_string(i + 1) + " 号　";
+    Pic += "</th>";
+  }
+  Pic += "</tr>";
+
+  Pic += "<tr>";
+  for (int i = 0; i < option().PlayerNum(); i++) {
+    //        if(i%2)
+    Pic += "<td>";
+    //        else
+    //            Pic+="<td bgcolor=\"#DCDCDC\">";
+    Pic += "30";
+    Pic += "</td>";
+  }
+  Pic += "</tr>";
+
+  std::string PreBoard = "";
+  PreBoard += "本局玩家序号如下：\n";
+  for (int i = 0; i < option().PlayerNum(); i++) {
+    PreBoard += std::to_string(i + 1) + " 号：" + PlayerName(i);
+
+    if (i != (int)option().PlayerNum() - 1) {
+      PreBoard += "\n";
+    }
+  }
+
+  Boardcast() << PreBoard;
+  Boardcast() << Markdown(Pic + "</table>");
+
+  return std::make_unique<RoundStage>(*this, ++round_);
+}
+
+MainStage::VariantSubStage MainStage::NextSubStage(RoundStage& sub_stage,
+                                                   const CheckoutReason reason) {
+  //    Boardcast()<<"alive"<<std::to_string(alive_);
+  if ((++round_) <= option().GET_VALUE(回合数) && alive_ > 1) {
+    return std::make_unique<RoundStage>(*this, round_);
+  }
+  //    Boardcast() << "游戏结束";
+  int maxHP = 0;
+  for (int i = 0; i < option().PlayerNum(); i++) {
+    maxHP = std::max(maxHP, (int)player_hp_[i]);
+  }
+  for (int i = 0; i < option().PlayerNum(); i++) {
+    if (maxHP == player_hp_[i]) {
+      // The last survival gain 5 scores.
+      player_scores_[i] += 5;
+    } else if (player_eli_[i] == 0) {
+      // The second last survival
+      player_scores_[i] += 3;
+    }
+  }
+  // Returning empty variant means the game will be over.
+  return {};
+}
+MainStageBase* MakeMainStage(MsgSenderBase& reply, GameOption& options, MatchBase& match) {
+  if (!options.ToValid(reply)) {
+    return nullptr;
+  }
+  return new MainStage(options, match);
+}
---
+++ games/bread_crisis/unittest.cc
@@ -0,0 +1,30 @@
+// Copyright (c) 2018-present, Chang Liu <github.com/slontia>. All rights reserved.
+//
+// This source code is licensed under LGPLv2 (found in the LICENSE file).
+
+#include "game_framework/unittest_base.h"
+
+// The first parameter is player number. It is a one-player game test.
+GAME_TEST(1, player_not_enough) {
+  ASSERT_FALSE(StartGame());  // according to |GameOption::ToValid|, the mininum player number is 3
+}
+
+// The first parameter is player number. It is a five-player game test.
+// GAME_TEST(5, simple_test)
+// {
+//     START_GAME();
+
+//     for (int pid = 0; pid < 4; ++pid) {
+//         ASSERT_PRI_MSG(OK, pid, "贼 " + std::to_string(pid + 2)); // we regard |READY| as |OK|
+//         here
+//     }
+//     ASSERT_PRI_MSG(CHECKOUT, 4, "贼 1");
+
+//     // The game is over, now we check each player's score.
+// }
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  return RUN_ALL_TESTS();
+}
