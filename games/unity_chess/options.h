ca999c4218c1c2ebe7ee52afab251e98da526dfd
jeffxzy <xuzhengyangjeff@126.com>
2023-01-31 19:21:34 +0800

    unity chess: bug fix

--- games/unity_chess/options.h
+++ games/unity_chess/options.h
@@ -1,3 +1,3 @@
 EXTEND_OPTION("回合数", 回合数, (ArithChecker<uint32_t>(1, 20, "回合数")), 3)
 EXTEND_OPTION("每回合时间限制", 时限, (ArithChecker<uint32_t>(10, 3600, "超时时间（秒）")), 180)
-EXTEND_OPTION("棋盘边长", 边长, (ArithChecker<uint32_t>(5, 9, "边长")), 6)
+EXTEND_OPTION("棋盘边长", 边长, (ArithChecker<uint32_t>(5, 15, "边长")), 6)
