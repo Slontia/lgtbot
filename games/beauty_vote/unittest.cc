40264eb09c6c6e2e1b3b7dcac2040ec2993b1c59
tiedanGH <2295824927@qq.com>
2023-09-03 22:26:38 +0800

    beauty_vote: add game test

--- games/beauty_vote/unittest.cc
+++ games/beauty_vote/unittest.cc
@@ -22,14 +22,62 @@ GAME_TEST(5, simple_test)
     // ASSERT_PUB_MSG(OK, 0, "回合数 1"); // set game option here
     START_GAME();
 
-    for (int pid = 0; pid < 4; ++pid) {
-        ASSERT_PRI_MSG(OK, pid, std::to_string(pid * 100)); // we regard |READY| as |OK| here
-    }
+    ASSERT_PRI_MSG(OK, 0, "16"); // we regard |READY| as |OK| here
+    ASSERT_PRI_MSG(OK, 1, "22");
+    ASSERT_PRI_MSG(OK, 2, "24");
+    ASSERT_PRI_MSG(OK, 3, "21");
     // When the last player is ready, the stage will be over.
-    ASSERT_PRI_MSG(CHECKOUT, 4, "400");
+    ASSERT_PRI_MSG(CHECKOUT, 4, "28");
+
+    ASSERT_PRI_MSG(OK, 0, "10");
+    ASSERT_PRI_MSG(OK, 1, "23");
+    ASSERT_PRI_MSG(OK, 2, "13");
+    ASSERT_PRI_MSG(OK, 3, "12");
+    ASSERT_PRI_MSG(CHECKOUT, 4, "8");
+
+    ASSERT_PRI_MSG(OK, 0, "7");
+    ASSERT_PRI_MSG(OK, 1, "21");
+    ASSERT_PRI_MSG(OK, 2, "7");
+    ASSERT_PRI_MSG(OK, 3, "7");
+    ASSERT_PRI_MSG(CHECKOUT, 4, "10");
+
+    ASSERT_PRI_MSG(OK, 0, "14");
+    ASSERT_PRI_MSG(OK, 1, "24");
+    ASSERT_PRI_MSG(OK, 2, "4");
+    ASSERT_PRI_MSG(OK, 3, "3");
+    ASSERT_PRI_MSG(CHECKOUT, 4, "10");
+
+    ASSERT_PRI_MSG(OK, 0, "11");
+    ASSERT_PRI_MSG(OK, 1, "27");
+    ASSERT_PRI_MSG(OK, 2, "11");
+    ASSERT_PRI_MSG(OK, 3, "8");
+    ASSERT_PRI_MSG(CHECKOUT, 4, "100");
+
+    ASSERT_PRI_MSG(OK, 0, "14");
+    ASSERT_PRI_MSG(OK, 1, "28");
+    ASSERT_PRI_MSG(OK, 2, "13");
+    ASSERT_PRI_MSG(OK, 3, "8");
+    ASSERT_PRI_MSG(CHECKOUT, 4, "18");
+
+    ASSERT_PRI_MSG(OK, 0, "9");
+    ASSERT_PRI_MSG(OK, 1, "29");
+    ASSERT_PRI_MSG(OK, 2, "9");
+    ASSERT_PRI_MSG(OK, 3, "6");
+    ASSERT_PRI_MSG(CHECKOUT, 4, "6");
+
+    ASSERT_PRI_MSG(OK, 0, "3");
+    ASSERT_PRI_MSG(OK, 1, "21");
+    ASSERT_PRI_MSG(OK, 2, "13");
+    ASSERT_PRI_MSG(OK, 3, "8");
+    ASSERT_PRI_MSG(CHECKOUT, 4, "7");
+
+    ASSERT_PRI_MSG(OK, 0, "8");
+    ASSERT_PRI_MSG(OK, 1, "11");
+    ASSERT_PRI_MSG(OK, 2, "6");
+    ASSERT_PRI_MSG(CHECKOUT, 3, "5");
 
     // The game is over, now we check each player's score.
-    ASSERT_SCORE(0, 100, 200, 300, 400);
+    ASSERT_SCORE(11, 10, 19, 11, 8);
 }
 
 } // namespace GAME_MODULE_NAME
