366eb993d2f151b68c05a1fa5696e7388d24c0d6
tiedan <2295824927@qq.com>
2024-05-04 00:59:11 +0800

    change copyright

--- games/gold_coins/unittest.cc
+++ games/gold_coins/unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-present, Chang Liu <github.com/slontia>. All rights reserved.
+// Copyright (c) 2018-present, JiaQi Yu <github.com/tiedanGH>. All rights reserved.
 //
 // This source code is licensed under LGPLv2 (found in the LICENSE file).
 
