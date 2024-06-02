608be61d1c27282207a0000f27e49b5a8b291648
tiedanGH <2295824927@qq.com>
2024-06-02 20:45:59 +0800

    naval_battle: new bosses & update mark commands

--- games/naval_battle/options.h
+++ games/naval_battle/options.h
@@ -6,11 +6,5 @@ EXTEND_OPTION("是否允许飞机互相重叠", 重叠, (BoolChecker("允许", "
 EXTEND_OPTION("设置命中要害是否有提示", 要害, AlterChecker<int>({{"有", 0}, {"无", 1}, {"首次", 2}}), 0)
 EXTEND_OPTION("连发次数", 连发, (ArithChecker<uint32_t>(1, 10, "次数")), 3)
 EXTEND_OPTION("初始随机侦察区域大小（默认为随机）", 侦察, (ArithChecker<uint32_t>(0, 30, "面积")), 100)
-EXTEND_OPTION("【BOSS挑战配置】</br>"
-              "设置时须按照下方的顺序，不能跳过其中的某一项，未设置则为默认</br>"
-              "[第1项] 挑战的BOSS类型：仅支持 1-2，输入其他为随机</br>"
-              "[第2项] 重叠：0 为不允许，大于0 均为允许</br>"
-              "[第3项] 要害：0 为有要害提示，1 为无要害提示，大于1 均为首要害提示</br>"
-              "[第4项] 连发次数：范围1-10，其他输入均为默认</br>"
-              "[第5项] 侦察区域大小：范围0-30，其他输入均为默认",
-              BOSS挑战, (RepeatableChecker<ArithChecker<int32_t>>(0, 100, "配置")), (std::vector<int32_t>{0}))
+EXTEND_OPTION("BOSS挑战类型：【快捷配置已不再此处支持】<br/>"
+              "如需使用快捷配置请使用「#规则 大海战」查看帮助", BOSS挑战, (ArithChecker<uint32_t>(0, 3, "类型")), 100)
