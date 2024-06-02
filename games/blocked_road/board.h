b3227214a3d0202d4ea62d092dde040e031c5fcb
tiedanGH <2295824927@qq.com>
2024-06-02 20:29:32 +0800

    blocked_road: new game

---
+++ games/blocked_road/board.h
@@ -0,0 +1,353 @@
+
+const string color_ch[2] = {"红", "黑"};
+const string direction_ch[5] = {{}, "上", "下", "左", "右"};
+
+class Board
+{
+public:
+
+    // 底色 0正常 1本回合行动
+    const string color[2] = {"#ECECEC", "#FFEEAD"};
+    // 棋盘大小 
+	int size = 4;
+    // 棋盘
+    int chess[20][20];
+    // 上一枚落子 
+	int lastX1, lastY1, lastX2, lastY2;
+    // 棋盘的markdown格式 
+	string grid[20][30];
+
+	// 玩家昵称
+	string name[2];
+	// 玩家分数及目标分
+	int score[2], targetScore;
+
+    // 初始化棋盘
+    void InitializeMap()
+    {
+        lastX1 = lastX2 = lastY1 = lastY2 = -1;
+        for(int j = 0; j <= size; j++)
+			for(int i = 0; i <= size; i++)
+				chess[i][j] = 0;
+		// 随机生成开局棋子
+		int temp = 0;
+		while (temp < 4) {
+			int X = rand() % size + 1;
+			int Y = rand() % size + 1;
+			if (chess[X][Y] == 0) {
+				chess[X][Y] = temp / 2 + 1;
+				temp++;
+			}
+		}
+    }
+
+	void ClearLast()
+	{
+		lastX1 = lastX2 = lastY1 = lastY2 = -1;
+	}
+
+    // 图形界面
+	string GetUI(bool show_bg)
+    {
+        // 初始化 
+		for(int i = 0; i <= size + 2; i++)
+			for(int j = 0; j <= size + 2; j++)
+				grid[i][j] = "";
+
+        // 用来填充的字符串 
+		string fill = "<font size=3>　</font>";
+
+        // 上下字母
+		for(int i = 1; i <= size; i++)
+		{
+			grid[i][0] += "<td bgcolor=\"#FFFFFF\"><font size=5>";
+			grid[i][0] += char(i + 'A' - 1);
+			grid[i][0] += "</font></td>";
+			
+			grid[i][size + 1] += "<td bgcolor=\"#FFFFFF\"><font size=5>";
+			grid[i][size + 1] += char(i + 'A' - 1);
+			grid[i][size + 1] += "</font></td>";
+			
+			// 最右侧的填充格 
+			grid[i][size + 2] += "<td bgcolor=\"#FFFFFF\">";
+			grid[i][size + 2] += fill;
+			grid[i][size + 2] += "</td>";
+		}
+		
+		// 左右数字 
+		for(int j = 1; j <= size; j++)
+		{
+			grid[0][j] += "<td bgcolor=\"#FFFFFF\"><font size=5>";
+			grid[0][j] += to_string(j);
+			grid[0][j] += "</font></td>";
+			
+			grid[size + 1][j] += "<td bgcolor=\"#FFFFFF\"><font size=5>";
+			grid[size + 1][j] += to_string(j);
+			grid[size + 1][j] += "</font></td>";
+			
+			// 最下方的填充格 
+			grid[size + 2][j] += "<td bgcolor=\"#FFFFFF\">";
+			grid[size + 2][j] += fill;
+			grid[size + 2][j] += "</td>";
+		}
+
+        // 棋盘边缘
+		grid[0][0] = grid[0][size + 1] = grid[0][size + 2] = 
+		grid[size + 1][0] = grid[size + 1][size + 1] = grid[size + 1][size + 2] = 
+		grid[size + 2][0] = grid[size + 2][size + 1] = grid[size + 2][size + 2] = 
+		"<td bgcolor=\"#FFFFFF\"><font size=7>" + fill + "</font></td>";
+
+        // 中间棋盘
+		for(int j = 1; j <= size; j++)
+		{
+	 		for(int i = 1; i <= size; i++)
+	 		{
+			 	grid[i][j] += "<td style=\"width: 60px; height: 60px;\" bgcolor=\"";
+			 	
+			 	// 背景色
+			 	if(((i == lastX1 && j == lastY1) || (i == lastX2 && j == lastY2)) && show_bg) {
+					grid[i][j] += color[1];
+				} else {
+                    grid[i][j] += color[0];
+                }
+			 	
+			 	// 棋子
+			 	grid[i][j] += "\"><font size=7>";
+			 	
+			 	string ch;
+			 	if(chess[i][j] == 0)
+				 	ch = fill;
+			 	if(chess[i][j] == 1)
+					ch = "<svg height=\"60px\" width=\"60px\"><circle cx=\"30\" cy=\"30\" r=\"18\" fill=\"#FF0000\"/></svg>";
+			 	if(chess[i][j] == 2)
+					ch = "<svg height=\"60px\" width=\"60px\"><circle cx=\"30\" cy=\"30\" r=\"18\" fill=\"#000000\"/></svg>";
+			 	grid[i][j] += ch;
+			 	
+			 	grid[i][j] += "</font></td>";
+			}
+		}
+
+        // 构造UI 
+		string UI = "";
+
+		// 玩家0 信息
+		UI += "<table style=\"text-align:center;margin:auto;\"><tbody>";
+        UI += "<tr><td>" + fill + "</td></tr>";
+        UI += "<tr><td style=\"width:500px\" bgcolor=\"#ECECEC\"><font size=4>" + name[0] + "</font></td></tr>";
+		UI += "<tr><td style=\"width:500px\" bgcolor=\"#ECECEC\"><font size=5><font color=\"FF0000\"><b>红方</b></font>分数：";
+		UI += "<font color=\"0000FF\">" + to_string(score[0]) + " / " + to_string(targetScore) + "</font></font></td></tr>";
+		UI += "</table>";
+
+		UI += "<table style=\"text-align:center;margin:auto;\"><tbody>";
+		
+		// 为保持对称，开头要额外增加一行，并且每行下方都额外增加一格
+		UI += "<tr><td bgcolor=\"#FFFFFF\">" + fill + "</td></tr>";
+		for(int j = 0; j <= size + 2; j++)
+		{
+			UI += "<tr>";
+			
+			// 额外增加一格 
+			UI += "<td bgcolor=\"#FFFFFF\">" + fill + "</td>";
+			
+	 		for(int i = 0; i <= size + 2; i++)
+	 		{
+	 			UI += grid[i][j];
+	 		}
+	 		UI += "</tr>";
+		}
+		UI += "</table>";
+
+		// 玩家1 信息
+		UI += "<table style=\"text-align:center;margin:auto;\"><tbody>";
+		UI += "<tr><td style=\"width:500px\" bgcolor=\"#ECECEC\"><font size=5><b>黑方</b>分数：";
+		UI += "<font color=\"0000FF\">" + to_string(score[1]) + " / " + to_string(targetScore) + "</font></font></td></tr>";
+        UI += "<tr><td style=\"width:500px\" bgcolor=\"#ECECEC\"><font size=4>" + name[1] + "</font></td></tr>";
+		UI += "<tr><td>" + fill + "</td></tr>";
+		UI += "</table>";
+		
+		return UI;
+    }
+
+	// 检查一个字符串是否是合法的位置 
+	string CheckCoordinate(string &s)
+	{
+		// 长度必须为2或3 
+		if (s.length() != 2 && s.length() != 3)
+		{
+			return "[错误] 坐标只能是长度不超过 3 的字符串，如：A1";
+		}
+		// 大小写不敏感 
+		if (s[0] <= 'z' && s[0] >= 'a')
+		{
+			s[0] = s[0] - 'a' + 'A';
+		}
+		// 检查是否为合法输入 
+		if (s[0] > 'Z' || s[0] < 'A' || s[1] > '9' || s[1] < '0' )
+		{
+			return "[错误] 请输入合法的字符串（字母+数字），如：A1";
+		}
+		if (s.length() == 3 && (s[2] > '9' || s[2] < '0'))
+		{
+			return "[错误] 请输入合法的字符串（字母+数字），如：A1";
+		}
+		// 转化
+        auto pos = TranString(s);
+		int nowY = pos.first, nowX = pos.second; 
+		// 检查是否越界
+		if (nowX < 1 || nowX > size || nowY < 1 || nowY > size) 
+		{
+			return "[错误] 你选择的位置超出了棋盘的大小";
+		}
+
+		return "OK";
+	}
+
+    // 将字符串转为一个位置pair。必须确保字符串是合法的再执行这个操作。 
+	static pair<int, int> TranString(string s)
+	{
+		int nowX = s[0] - 'A' + 1, nowY = s[1] - '0'; 
+		if (s.length() == 3)
+		{
+			nowY = (s[1] - '0') * 10 + s[2] - '0';
+		}
+		pair<int, int> ret;
+		ret.first = nowX;
+		ret.second = nowY;
+		return ret;
+	}
+
+    // 开局为对手落子（此处已为对手代号）
+	string PlaceChess(string s, int player)
+	{
+        string ret = CheckCoordinate(s);
+        if (ret != "OK") return ret;
+
+		player += 1;
+		auto pos = TranString(s);
+		int x = pos.first, y = pos.second; 
+		if(chess[x][y] != 0)
+		{
+			return "[错误] 这个位置已经有棋子了";
+		}
+		
+		lastX1 = pos.first, lastY1 = pos.second;
+		lastX2 = -1, lastY2 = -1;
+		chess[pos.first][pos.second] = player;
+
+		if (WinCheck() != -1) {
+			ClearLast();
+			chess[pos.first][pos.second] = 0;
+			return "[错误] 落子失败：当前在为对手落子，此位置会让对手达成三连";
+		}
+		
+		return "OK"; 
+	}
+
+    // 记录棋子移动前后位置（尝试移动回合）
+    string RecordChessMove(string s1, string s2, int player)
+    {
+        string ret1 = CheckCoordinate(s1);
+		string ret2 = CheckCoordinate(s2);
+        if (ret1 != "OK") return ret1;
+		if (ret2 != "OK") return ret2;
+
+        player += 1;
+        auto pos1 = TranString(s1);
+		auto pos2 = TranString(s2);
+        int x1 = pos1.first, x2 = pos2.first, y1 = pos1.second, y2 = pos2.second;
+
+        if ((x1 != x2 && y1 != y2) || (x1 == x2 && y1 == y2))
+		{
+			return "[错误] 这不是一个合法的移动";
+		}
+        if (chess[x1][y1] == 0)
+		{
+			return "[错误] 这个位置没有棋子";
+		}
+        if (chess[x1][y1] != player)
+        {
+            return "[错误] 这个位置不是您的棋子";
+        }
+
+        int addx = 0, addy = 0;
+		if (x1 < x2) addx = 1;
+		if (x1 > x2) addx = -1;
+		if (y1 < y2) addy = 1;
+		if (y1 > y2) addy = -1;
+
+		while(x1 != x2 || y1 != y2)
+		{
+			x1 = x1 + addx;
+			y1 = y1 + addy;
+			if (chess[x1][y1] != 0)
+			{
+				return "[错误] 移动的路径上有别的棋子";
+			}
+		}
+
+		x1 = pos1.first; x2 = pos2.first; y1 = pos1.second; y2 = pos2.second;
+        // last1移动前位置 last2移动后位置
+        lastX1 = x1, lastY1 = y1;
+		lastX2 = x2, lastY2 = y2;
+
+        return "OK"; 
+	}
+
+	// 返回当回合猜测的结果
+	int GuessResult(int direction)
+	{
+		int addx = 0, addy = 0;
+		if (lastX1 < lastX2) addx = 1;
+		if (lastX1 > lastX2) addx = -1;
+		if (lastY1 < lastY2) addy = 1;
+		if (lastY1 > lastY2) addy = -1;
+
+		if (addy < 0) {
+			if (direction == 1) return 0;
+			else return 1;
+		}
+		if (addy > 0) {
+			if (direction == 2) return 0;
+			else return 2;
+		}
+		if (addx < 0) {
+			if (direction == 3) return 0;
+			else return 3;
+		}
+		if (addx > 0) {
+			if (direction == 4) return 0;
+			else return 4;
+		}
+
+		return 0;
+	}
+
+	// 检查是否有玩家三连胜利
+    int WinCheck() {
+		int winner = -1;
+		auto c = chess;
+		for (int i = 1; i <= size; i++)
+		{
+			for (int j = 1; j <= size; j++)
+			{
+				if (c[i][j] == 0) continue;
+
+				if ((c[i][j] == c[i + 1][j] && c[i][j] == c[i + 2][j]) ||
+					(c[i][j] == c[i][j + 1] && c[i][j] == c[i][j + 2]) ||
+					(c[i][j] == c[i + 1][j + 1] && c[i][j] == c[i + 2][j + 2]))
+				{
+					winner = c[i][j] - 1; 
+				}
+				// 特判，不然可能访问到负数位置 
+				if (j >= 3)
+				{
+					if (c[i][j] == c[i + 1][j - 1] && c[i][j] == c[i + 2][j - 2])
+					{
+						winner = c[i][j] - 1; 
+					}
+				}
+			}
+		}
+		return winner;
+	}
+};
---
+++ games/blocked_road/mygame.cc
@@ -0,0 +1,369 @@
+// Copyright (c) 2018-present, JiaQi Yu <github.com/tiedanGH>. All rights reserved.
+//
+// This source code is licensed under LGPLv2 (found in the LICENSE file).
+
+#include "game_framework/stage.h"
+#include "game_framework/util.h"
+#include "utility/html.h"
+
+using namespace std;
+
+#include "board.h"
+
+namespace lgtbot {
+
+namespace game {
+
+namespace GAME_MODULE_NAME {
+
+class MainStage;
+template <typename... SubStages> using SubGameStage = StageFsm<MainStage, SubStages...>;
+template <typename... SubStages> using MainGameStage = StageFsm<void, SubStages...>;
+
+const std::string k_game_name = "此路不通"; // the game name which should be unique among all the games
+uint64_t MaxPlayerNum(const MyGameOptions& options) { return 2; } // 0 indicates no max-player limits
+uint32_t Multiple(const MyGameOptions& options) { return 1; } // the default score multiple for the game, 0 for a testing game, 1 for a formal game, 2 or 3 for a long formal game
+const std::string k_developer = "铁蛋";
+const std::string k_description = "猜测并阻止对手移动棋子，使棋子达成3连的游戏";
+const MutableGenericOptions k_default_generic_options;
+const std::vector<RuleCommand> k_rule_commands = {};
+
+bool AdaptOptions(MsgSenderBase& reply, MyGameOptions& game_options, const GenericOptions& generic_options_readonly, MutableGenericOptions& generic_options)
+{
+    if (generic_options_readonly.PlayerNum() != 2) {
+        reply() << "该游戏为双人游戏，必须为2人参加，当前玩家数为 " << generic_options_readonly.PlayerNum();
+        return false;
+    }
+    return true;
+}
+
+const std::vector<InitOptionsCommand> k_init_options_commands = {
+    InitOptionsCommand("独自一人开始游戏",
+            [] (MyGameOptions& game_options, MutableGenericOptions& generic_options)
+            {
+                generic_options.bench_computers_to_player_num_ = 2;
+                return NewGameMode::SINGLE_USER;
+            },
+            VoidChecker("单机")),
+};
+
+// ========== GAME STAGES ==========
+
+class StartStage;
+class RoundStage;
+
+class MainStage : public MainGameStage<StartStage, RoundStage>
+{
+  public:
+    MainStage(StageUtility&& utility)
+        : StageFsm(std::move(utility), MakeStageCommand(*this, "查看当前游戏进展情况", &MainStage::Status_, VoidChecker("赛况")))
+        , round_(0)
+        , player_scores_(Global().PlayerNum(), 0)
+    {}
+
+    virtual int64_t PlayerScore(const PlayerID pid) const override { return player_scores_[pid]; }
+
+    std::vector<int64_t> player_scores_;
+
+    // 当前行动玩家
+    PlayerID currentPlayer;
+    // 棋盘
+    Board board;
+    // 回合数 
+	int round_;
+    // 判定是否是超时淘汰
+    int timeout[2];
+
+  private:
+    CompReqErrCode Status_(const PlayerID pid, const bool is_public, MsgSenderBase& reply)
+    {
+        reply() << Markdown(board.GetUI(false));
+        // Returning |OK| means the game stage
+        return StageErrCode::OK;
+    }
+
+    void FirstStageFsm(SubStageFsmSetter setter)
+    {
+        // 随机先后手
+        srand((unsigned int)time(NULL));
+        currentPlayer = rand() % 2;
+
+        board.targetScore = GAME_OPTION(目标);
+        board.size = GAME_OPTION(边长);
+        for (PlayerID pid = 0; pid < Global().PlayerNum(); ++pid) {
+            board.name[pid] = Global().PlayerName(pid);
+            if (board.name[pid][0] == '<') {
+                board.name[pid] = board.name[pid].substr(1, board.name[pid].size() - 2);
+            }
+            board.score[pid] = 0;
+            timeout[pid] = 0;
+        }
+        board.InitializeMap();
+
+        setter.Emplace<StartStage>(*this, ++round_);
+    }
+
+    void NextStageFsm(StartStage& sub_stage, const CheckoutReason reason, SubStageFsmSetter setter)
+    {
+        // 超时直接结束游戏
+        if (timeout[0] || timeout[1]) {
+            if (timeout[0]) player_scores_[0] = -1;
+            if (timeout[1]) player_scores_[1] = -1;
+            Global().Boardcast() << Markdown(board.GetUI(false));
+            return;
+        }
+        currentPlayer = 1- currentPlayer;
+        if ((++round_) <= (GAME_OPTION(棋子) - 2) * 2) {
+            setter.Emplace<StartStage>(*this, round_);
+            return;
+        }
+        round_ = 1;
+        setter.Emplace<RoundStage>(*this, round_);
+    }
+
+    void NextStageFsm(RoundStage& sub_stage, const CheckoutReason reason, SubStageFsmSetter setter)
+    {
+        currentPlayer = 1- currentPlayer;
+        if ((++round_) <= GAME_OPTION(回合数) && !timeout[0] && !timeout[1] && !player_scores_[0] && !player_scores_[1]) {
+            setter.Emplace<RoundStage>(*this, round_);
+            return;
+        }
+        if (round_ > GAME_OPTION(回合数)) {
+            Global().Boardcast() << "回合数已达上限，游戏平局！";
+        }
+
+        if (timeout[0]) player_scores_[0] = -1;
+        if (timeout[1]) player_scores_[1] = -1;
+        
+        Global().Boardcast() << Markdown(board.GetUI(false));
+    }
+};
+
+class StartStage : public SubGameStage<>
+{
+   public:
+    StartStage(MainStage& main_stage, const uint64_t round)
+            : StageFsm(main_stage, "落子阶段",
+                    MakeStageCommand(*this, "为对手落子", &StartStage::PlaceChess_, AnyArg("位置", "A1")))
+    {}
+
+    virtual void OnStageBegin() override
+    {
+        Global().Boardcast() << Markdown(Main().board.GetUI(true));
+        Main().board.ClearLast();
+        if (Main().round_ == 1) {
+            Global().Boardcast() << "游戏开始！\n" << At(Main().currentPlayer) << " 先手，执" << color_ch[Main().currentPlayer] << "。请为对手放置棋子";
+        } else {
+            Global().Boardcast() << "请 " << color_ch[Main().currentPlayer] << "方" << At(Main().currentPlayer) << " 行动，为对手放置棋子";
+        }
+        Global().SetReady(1 - Main().currentPlayer);
+        Global().StartTimer(GAME_OPTION(时限));
+    }
+
+   private:
+    AtomReqErrCode PlaceChess_(const PlayerID pid, const bool is_public, MsgSenderBase& reply, const string str)
+    {
+        if (pid != Main().currentPlayer) {
+            reply() << "[错误] 当前不是您的回合";
+            return StageErrCode::FAILED;
+        }
+        
+        string result = Main().board.PlaceChess(str, 1 - Main().currentPlayer);
+        if (result != "OK") {
+            reply() << result;
+            return StageErrCode::FAILED;
+        }
+        
+        return StageErrCode::CHECKOUT;
+    }
+
+    virtual CheckoutErrCode OnStageTimeout() override
+    {
+        Global().Boardcast() << color_ch[Main().currentPlayer] << "方" << At(Main().currentPlayer) << " 超时判负。";
+        Main().timeout[Main().currentPlayer] = 1;
+        return StageErrCode::CHECKOUT;
+    }
+
+    virtual CheckoutErrCode OnPlayerLeave(const PlayerID pid) override
+    {
+        Global().Boardcast() << color_ch[pid] << "方" << At(PlayerID(pid)) << " 强退认输。";
+        Main().timeout[pid] = 1;
+        return StageErrCode::CHECKOUT;
+    }
+
+    virtual AtomReqErrCode OnComputerAct(const PlayerID pid, MsgSenderBase& reply) override
+    {
+        if (pid == Main().currentPlayer) {
+            string result;
+            while (result != "OK") {
+                int X = rand() % Main().board.size + 1;
+                int Y = rand() % Main().board.size + 1;
+                result = Main().board.PlaceChess(string(1, 'A' + X - 1) + to_string(Y), 1 - Main().currentPlayer);
+            }
+            return StageErrCode::READY;
+        }
+        return StageErrCode::OK;
+    }
+};
+
+class RoundStage : public SubGameStage<>
+{
+   public:
+    RoundStage(MainStage& main_stage, const uint64_t round)
+            : StageFsm(main_stage, "第 " + std::to_string(round) + " 回合" ,
+                MakeStageCommand(*this, "移动棋子", &RoundStage::MoveChess_, AnyArg("起始位置", "A1"), AnyArg("结束位置", "A2")),
+                MakeStageCommand(*this, "猜测对手的移动方向", &RoundStage::GuessMove_, AlterChecker<int>({{"上", 1}, {"下", 2}, {"左", 3}, {"右", 4}})))
+    {}
+
+    int guess_;
+
+    virtual void OnStageBegin() override
+    {
+        Global().Boardcast() << Markdown(Main().board.GetUI(true));
+        Global().Boardcast() << "请 " << color_ch[Main().currentPlayer] << "方" << At(Main().currentPlayer) << " 私信裁判移动棋子\n"
+                             << "请 " << At(PlayerID(1 - Main().currentPlayer)) << " 设定本回合的禁走方向\n"
+                             << "时限 " << GAME_OPTION(时限) << " 秒，超时未行动即判负";
+        Global().Tell(0) << Markdown(Main().board.GetUI(false));
+        Global().Tell(1) << Markdown(Main().board.GetUI(false));
+        Global().StartTimer(GAME_OPTION(时限));
+    }
+
+   private:
+    AtomReqErrCode MoveChess_(const PlayerID pid, const bool is_public, MsgSenderBase& reply, const string str1, const string str2)
+    {
+        if (pid != Main().currentPlayer) {
+            reply() << "[错误] 本回合您为猜测者，无法移动棋子";
+            return StageErrCode::FAILED;
+        }
+        if (is_public) {
+            reply() << "[错误] 请私信裁判移动棋子";
+            return StageErrCode::FAILED;
+        }
+        string result = Main().board.RecordChessMove(str1, str2, Main().currentPlayer);
+        if (result != "OK") {
+            reply() << result;
+            return StageErrCode::FAILED;
+        }
+        reply() << "行动完成：" << str1 << " → " << str2;
+        return StageErrCode::READY;
+    }
+
+    AtomReqErrCode GuessMove_(const PlayerID pid, const bool is_public, MsgSenderBase& reply, const int guess)
+    {
+        if (pid == Main().currentPlayer) {
+            reply() << "[错误] 本回合您为移动者，无法进行猜测";
+            return StageErrCode::FAILED;
+        }
+        if (is_public) {
+            reply() << "[错误] 请私信裁判进行猜测";
+            return StageErrCode::FAILED;
+        }
+        guess_ = guess;
+        reply() << "行动完成：禁走方向为" << direction_ch[guess];
+        return StageErrCode::READY;
+    }
+
+    virtual CheckoutErrCode OnStageTimeout() override
+    {
+        for (PlayerID pid = 0; pid < Global().PlayerNum(); ++pid) {
+            if (!Global().IsReady(pid)) {
+                Global().Boardcast() << color_ch[pid] << "方" << At(pid) << " 超时判负。";
+                Main().timeout[pid] = 1;
+            }
+        }
+        return StageErrCode::CHECKOUT;
+    }
+
+    virtual CheckoutErrCode OnPlayerLeave(const PlayerID pid) override
+    {
+        Global().Boardcast() << color_ch[pid] << "方" << At(PlayerID(pid)) << " 强退认输。";
+        Main().timeout[pid] = 1;
+        return StageErrCode::CHECKOUT;
+    }
+
+    virtual CheckoutErrCode OnStageOver() override
+    {
+        string start_pos = string(1, 'A' + Main().board.lastX1 - 1) + to_string(Main().board.lastY1);
+        string end_pos = string(1, 'A' + Main().board.lastX2 - 1) + to_string(Main().board.lastY2);
+        
+        // 结算猜测结果
+        int result = Main().board.GuessResult(guess_);
+        if (result == 0) {
+            // 猜测正确，阻止移动并获得1分
+            Main().board.score[1 - Main().currentPlayer]++;
+
+            Global().Boardcast() << Markdown(Main().board.GetUI(true));
+
+            Global().Boardcast() << At(Main().currentPlayer) << " 将棋子从 " << start_pos << " 移动至 " << end_pos << "\n"
+                                 << At(PlayerID(1 - Main().currentPlayer)) << " 设定禁止向" << direction_ch[guess_] << "走\n"
+                                 << "【猜测成功】阻止移动并获得 1 分，当前分数为 " << Main().board.score[1 - Main().currentPlayer];
+        } else {
+            // 猜测失败，正常执行移动
+            Main().board.chess[Main().board.lastX1][Main().board.lastY1] = 0;
+            Main().board.chess[Main().board.lastX2][Main().board.lastY2] = Main().currentPlayer + 1;
+
+            Global().Boardcast() << Markdown(Main().board.GetUI(true));
+
+            Global().Boardcast() << At(Main().currentPlayer) << " 将棋子从 " << start_pos << " 移动至 " << end_pos << "\n"
+                                 << At(PlayerID(1 - Main().currentPlayer)) << " 设定禁止向" << direction_ch[guess_] << "走\n"
+                                 << "猜测失败，实际移动方向为" << direction_ch[result];
+        }
+
+        Main().board.ClearLast();
+
+        // 判断当前胜负
+        if (Main().board.score[1 - Main().currentPlayer] == GAME_OPTION(目标)) {
+            Main().player_scores_[1 - Main().currentPlayer] = 1;
+            Global().Boardcast() << color_ch[1 - Main().currentPlayer] << "方" << At(PlayerID(1 - Main().currentPlayer)) << " 分数达到了目标分，获得胜利！";
+        }
+        int winner = Main().board.WinCheck();
+        if (winner != -1) {
+            Main().player_scores_[Main().currentPlayer] = 1;
+            Global().Boardcast() << color_ch[Main().currentPlayer] << "方" << At(Main().currentPlayer) << " 棋子达成三连，获得胜利！";
+        }
+
+        return StageErrCode::CHECKOUT;
+    }
+    
+    virtual AtomReqErrCode OnComputerAct(const PlayerID pid, MsgSenderBase& reply) override
+    {
+        if (Global().IsReady(pid)) {
+            return StageErrCode::OK;
+        }
+        if (pid == Main().currentPlayer) {
+            int X, Y, addx, addy;
+            string result;
+            while (result != "OK") {
+                int c = rand() % GAME_OPTION(棋子) + 1;
+                for (int i = 1; i <= Main().board.size; i++) {
+                    for (int j = 1; j <= Main().board.size; j++) {
+                        if (Main().board.chess[i][j] == pid + 1 && c >= 0) {
+                            c--; X = i; Y = j;
+                        }
+                    }
+                }
+                addx = addy = 0;
+                if (rand() % 2) {
+                    addx = rand() % 2 == 1 ? 1 : -1;
+                } else {
+                    addy = rand() % 2 == 1 ? 1 : -1;
+                }
+                string start_pos = string(1, 'A' + X - 1) + to_string(Y);
+                string end_pos = string(1, 'A' + X + addx - 1) + to_string(Y + addy);
+                result = Main().board.RecordChessMove(start_pos, end_pos, pid);
+            }
+        } else {
+            guess_ = rand() % 4 + 1;
+        }
+        return StageErrCode::READY;
+    }
+};
+
+auto* MakeMainStage(MainStageFactory factory) { return factory.Create<MainStage>(); }
+
+} // namespace GAME_MODULE_NAME
+
+} // namespace game
+
+} // namespace lgtbot
+
